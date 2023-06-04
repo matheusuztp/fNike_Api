262814c3c37a160d09f8cd7daf0a58f1a2913e4f
matheusuztp <matheusuztp@hotmail.com>
2023-06-04 17:26:12 -0300

    Inserindo metodos controladores da API para produtos

---
+++ NikeAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NikeAPI.Context;
+using NikeAPI.Domain;
+
+namespace NikeAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            if(produtos.Any())
+                return produtos;
+            else
+                return NotFound("Produtos não encontrados...");
+            
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.Id == id);
+
+            if (produto == null)
+                return NotFound("Produto não encontrado...");
+            else
+                return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+
+        }
+    }
+}
